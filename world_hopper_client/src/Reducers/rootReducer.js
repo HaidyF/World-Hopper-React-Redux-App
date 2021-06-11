1b9213c99cda0ba3082087237ec724815a729eae
HaidyF <69495135+HaidyF@users.noreply.github.com>
2021-06-11 20:40:09 +0200

    updates

--- world_hopper_client/src/Reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-// import { combineReducers } from 'redux'
-// import { CountriesReducer } from './CountriesReducer'
-// import { UsersReducer } from './UsersReducer'
-
-// export const rootReducer = combineReducers({
-//     countries: CountriesReducer,
-//     users: UsersReducer
-// })
